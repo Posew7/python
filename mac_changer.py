75c49ff7aa37d498d9a7afddebf6efe12c26ed13
posew7 <posew7@gmail.com>
2019-02-27 10:37:49 -0500

    mac_changer

--- mac_changer.py
+++ mac_changer.py
@@ -1,23 +1,34 @@
 import subprocess
 import optparse
+import re
 
-def user_input():
+def get_user_input():
     parse_object = optparse.OptionParser()
-    parse_object.add_option("-i","--interface",dest="interface",help="interface to change!")
-    parse_object.add_option("-m","--mac",dest="mac",help="new mac adress")
-
+    parse_object.add_option("-i","--interface",dest="interface",help="change to interface")
+    parse_object.add_option("-m","--mac",dest="mac_address",help="new mac address")
     return parse_object.parse_args()
 
-def mac_changer(interface,mac):
-    subprocess.call(["ifconfig",interface,"down"])
-    subprocess.call(["ifconfig",interface,"hw","ether",mac])
-    subprocess.call(["ifconfig",interface,"up"])
+def change_mac_address(p_interface,p_mac_address):
+    subprocess.call(["ifconfig",p_interface,"down"])
+    subprocess.call(["ifconfig",p_interface,"hw","ether",p_mac_address])
+    subprocess.call(["ifconfig",p_interface,"up"])
 
-    subprocess.call(["ifconfig",interface])
+def control_new_mac(p_interface):
+    ifconfig = subprocess.check_output(["ifconfig",p_interface])
+    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(ifconfig))
 
+    if new_mac:
+        return new_mac.group(0)
+    else:
+        return None
 
+print("mac changer started !")
+(user_inputs,arguments) = get_user_input()
+change_mac_address(user_inputs.interface,user_inputs.mac_address)
+finalized_mac = control_new_mac(str(user_inputs.interface))
 
-(user_inputs,arguments) = user_input()
-mac_changer(user_inputs.interface, user_inputs.mac)
-
-
+if finalized_mac == user_inputs.mac_address:
+    print("success\n\n")
+    subprocess.call(["ifconfig",user_inputs.interface])
+else:
+    print("error")
